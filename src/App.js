4349b7906ff0bf3f35767d183bb0bff832085bbf
Chaoling Lu <37680291+flowersmiling@users.noreply.github.com>
2023-04-15 16:14:58 -0600

    question2 (#4)

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
+      <Route path='/' element={<Home />} />
         <Route path='/home' element={<Home />} />
         <Route path='/universities' element={<Universities />} />
         <Route path='/postal' element={<Postal />} />
